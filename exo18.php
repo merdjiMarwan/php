9e40fcdaaee37b783dfbcbf011d6c9b76e67a5be
merdjiMarwan <marwan.merdji@lycee-jeanrostand.fr>
2024-10-09 16:40:29 +0200

    serie1

---
+++ exo18.php
@@ -0,0 +1,13 @@
+<?php
+
+function verificationPassword($arg){
+    if (strlen($arg)>=8 && strpbrk($arg, '0123456789') && strpbrk($arg, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')  && 
+    strpbrk($arg, 'abcdefghijklmnopqrstuvwxyz')  ){
+        return true;
+    }else{
+        return false;
+    }
+}
+echo verificationPassword('12345678Mm')
+
+?>
